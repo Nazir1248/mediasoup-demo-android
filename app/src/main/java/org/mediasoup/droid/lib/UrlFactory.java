0a62b948f20720aa9b4c20d4c319560902f0298c
nazir <naziransari1248@gmmail.com>
2026-01-31 01:58:47 +0530

    fixed the gradle

--- app/src/main/java/org/mediasoup/droid/lib/UrlFactory.java
+++ app/src/main/java/org/mediasoup/droid/lib/UrlFactory.java
@@ -1,33 +1,79 @@
+//package org.mediasoup.droid.lib;
+//
+//import java.util.Locale;
+//
+//public class UrlFactory {
+//
+//  private static final String HOSTNAME = "v3demo.mediasoup.org";
+//  //  private static final String HOSTNAME = "192.168.1.103";
+//  private static final int PORT = 4443;
+//
+//  public static String getInvitationLink(String roomId, boolean forceH264, boolean forceVP9) {
+//    String url = String.format(Locale.US, "https://%s/?roomId=%s", HOSTNAME, roomId);
+//    if (forceH264) {
+//      url += "&forceH264=true";
+//    } else if (forceVP9) {
+//      url += "&forceVP9=true";
+//    }
+//    return url;
+//  }
+//
+//  public static String getProtooUrl(
+//      String roomId, String peerId, boolean forceH264, boolean forceVP9) {
+//    String url =
+//        String.format(
+//            Locale.US, "wss://%s:%d/?roomId=%s&peerId=%s", HOSTNAME, PORT, roomId, peerId);
+//    if (forceH264) {
+//      url += "&forceH264=true";
+//    } else if (forceVP9) {
+//      url += "&forceVP9=true";
+//    }
+//    return url;
+//  }
+//}
+
+
 package org.mediasoup.droid.lib;
 
 import java.util.Locale;
 
 public class UrlFactory {
 
-  private static final String HOSTNAME = "v3demo.mediasoup.org";
-  //  private static final String HOSTNAME = "192.168.1.103";
-  private static final int PORT = 4443;
+    // 🔴 CHANGE: use your LOCAL server IP
+    private static final String HOSTNAME = "172.25.97.125";
 
-  public static String getInvitationLink(String roomId, boolean forceH264, boolean forceVP9) {
-    String url = String.format(Locale.US, "https://%s/?roomId=%s", HOSTNAME, roomId);
-    if (forceH264) {
-      url += "&forceH264=true";
-    } else if (forceVP9) {
-      url += "&forceVP9=true";
-    }
-    return url;
-  }
+    // mediasoup-demo server expects this port
+    private static final int PORT = 4443;
 
-  public static String getProtooUrl(
-      String roomId, String peerId, boolean forceH264, boolean forceVP9) {
-    String url =
-        String.format(
-            Locale.US, "wss://%s:%d/?roomId=%s&peerId=%s", HOSTNAME, PORT, roomId, peerId);
-    if (forceH264) {
-      url += "&forceH264=true";
-    } else if (forceVP9) {
-      url += "&forceVP9=true";
+    public static String getInvitationLink(String roomId, boolean forceH264, boolean forceVP9) {
+        String url = String.format(Locale.US, "http://%s/?roomId=%s", HOSTNAME, roomId);
+        if (forceH264) {
+            url += "&forceH264=true";
+        } else if (forceVP9) {
+            url += "&forceVP9=true";
+        }
+        return url;
     }
-    return url;
-  }
+
+    public static String getProtooUrl(
+            String roomId, String peerId, boolean forceH264, boolean forceVP9) {
+
+        // 🔴 CHANGE: wss:// ➜ ws:// (NO TLS for local dev)
+        String url =
+                String.format(
+                        Locale.US,
+                        "ws://%s:%d/?roomId=%s&peerId=%s",
+                        HOSTNAME,
+                        PORT,
+                        roomId,
+                        peerId
+                );
+
+        if (forceH264) {
+            url += "&forceH264=true";
+        } else if (forceVP9) {
+            url += "&forceVP9=true";
+        }
+        return url;
+    }
 }
